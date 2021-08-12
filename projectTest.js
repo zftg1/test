e1c6deaca48d956a88f8afcd4d59f0f38359421c
Dre <acab1232@protonmail.com>
2021-08-12 15:42:03 +0300

    practice 2, rewrited with functions

--- projectTest.js
+++ projectTest.js
@@ -1,14 +1,18 @@
 'use strict';
 
-//PRACTICE 1.2
-// 1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-// 2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-// отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
-// возвращаем пользователя к вопросам опять. (К любой строке можно обратиться как 
-// str.length - и получить её длину)
+//PRACTICE 1.3
+//1
+let numberOfFilms;
 
-// MAIN OBJECT
+// const start = () => {
+//     numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+//     while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+//         numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+//     }
+// };
+
+// start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -22,35 +26,58 @@ const personalMovieDB = {
     private: false,
 };
 
-for (let i = 0; i < 2; i++){
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = +prompt('На сколько оцените его?', '');
-          if (a != null && b != null && a != '' && b != '' && a.length < 50){
-              personalMovieDB.movies[a] = b;
-              console.log('done!');
-          } else {
-              console.log('Error');
-              i--;
-          }
-}
+// const rememberMyFilms = () => {
+//     for (let i = 0; i < 2; i++){
+//         const a = prompt('Один из последних просмотренных фильмов?', ''),
+//               b = +prompt('На сколько оцените его?', '');
+//               if (a != null && b != null && a != '' && b != '' && a.length < 50){
+//                   personalMovieDB.movies[a] = b;
+//                   console.log('done!');
+//               } else {
+//                   console.log('Error');
+//                   i--;
+//               }
+//     }    
+// };
 
-// 3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-// "Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
-// "Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+// rememberMyFilms();
 
-const checkCount = (value) => {
-    if (value < 10) {
-        alert('Просмотрено довольно мало фильмов');
-    } else if (value >= 10 && value <= 30) {
-        alert('Вы классический зритель');
-    } else if (value > 30) {
-        alert('Вы киноман');
-    } else {
-        alert('Произошла ошибка');
+// const detectPersonalLevel = () => {
+//     if (personalMovieDB.count < 10) {
+//         alert('Просмотрено довольно мало фильмов');
+//     } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+//         alert('Вы классический зритель');
+//     } else if (personalMovieDB.count > 30) {
+//         alert('Вы киноман');
+//     } else {
+//         alert('Произошла ошибка');
+//     }
+// };
+
+// detectPersonalLevel();
+
+// const showMyDB = () => {
+//     if (personalMovieDB.private === false) {
+//         console.log(personalMovieDB);
+//     }
+// };
+
+// showMyDB();
+
+let genresAnswer;
+const writeYourGenres = () => {
+    while (genresAnswer <= 3) {
+        let filmNumber;
+        for (filmNumber = 0; filmNumber <= 3; filmNumber++) {
+            return filmNumber; 
+        }
+        genresAnswer = prompt(`Ваш любимый жанр под номером ${filmNumber}`);
     }
+
+    return personalMovieDB.genres.push(genresAnswer);
 };
 
-checkCount(personalMovieDB.count);
+writeYourGenres(genresAnswer);
 
 //debug test
 console.log(personalMovieDB);
